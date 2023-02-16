dc07c53aa4292d4e41c4aaa69064a3a80f1e6df1
Shivansh-soni <shivanshsoni.indore@gmail.com>
2023-02-16 10:24:11 +0530

    Margin issues

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { google, slack, atlassian, dropbox, shopify } from "./imports";
 import "./brand.css";
+import { motion } from "framer-motion";
 
 const Brand = () => (
-  <div className="gpt3__brand section__padding">
+  <motion.div
+    className="gpt3__brand section__padding"
+    initial={{ opacity: 0, x: "100%" }}
+    animate={{ opacity: 1, x: "0%" }}
+    // whileInView={{ opacity: 1, x: "0%" }}
+    transition={{ duration: "1.5", ease: "easeInOut" }}
+  >
     <div>
       <img src={google} alt="logo" />
     </div>
@@ -19,7 +26,7 @@ const Brand = () => (
     <div>
       <img src={shopify} alt="logo" />
     </div>
-  </div>
+  </motion.div>
 );
 
 export default Brand;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,25 +2,60 @@ import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./navbar.css";
+import { motion } from "framer-motion";
 
 const Menu = () => (
-  <>
-    <p>
-      <a href="#home">Home</a>
-    </p>
-    <p>
-      <a href="#wgpt3">What is GPT?</a>
-    </p>
-    <p>
-      <a href="#possibility">Open AI</a>
-    </p>
-    <p>
-      <a href="#features">Case Studies</a>
-    </p>
-    <p>
-      <a href="#blog">Library</a>
-    </p>
-  </>
+  <motion.div
+    initial={{ opacity: 0, y: "0%" }}
+    animate={{ opacity: 1, y: "2%" }}
+    transition={{ delay: ".25" }}
+  >
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.25 }}
+    >
+      <p>
+        <a href="#home">Home</a>
+      </p>
+    </motion.div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5 }}
+    >
+      <p>
+        <a href="#wgpt3">What is GPT?</a>
+      </p>
+    </motion.div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.75 }}
+    >
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+    </motion.div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1 }}
+    >
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+    </motion.div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.25 }}
+    >
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </motion.div>
+  </motion.div>
 );
 
 const Navbar = () => {
@@ -70,13 +105,18 @@ const Navbar = () => {
         )}
 
         {toggleMenu && (
-          <div className="gpt3__navbar-menu_container scale-up-center">
+          <motion.div
+            className="gpt3__navbar-menu_container "
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: ".75", ease: "easeInOut" }}
+          >
             <Menu />
             <div className="gpt3__navbar-menu_container-links-sign">
               <p>Sign in</p>
               <button type="button"> Sign up </button>
             </div>
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
